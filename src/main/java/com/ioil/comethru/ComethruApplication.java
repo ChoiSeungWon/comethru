c3fc56f307016704148aa1892f2acc8fd00b967d
choiseungwon <b088081@gmail.com>
2020-04-19 21:08:07 +0900

    Initial commit

---
+++ src/main/java/com/ioil/comethru/ComethruApplication.java
@@ -0,0 +1,13 @@
+package com.ioil.comethru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComethruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComethruApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ioil/comethru/ComethruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioil.comethru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComethruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
